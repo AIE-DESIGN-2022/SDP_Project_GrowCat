80d43a5ea728274d5eb36d318d965232b1087f05
AIE\s213366 <s213366@students.aie.edu.au>
2022-06-30 15:19:56 +0930

    ADDED UI support, clicking buttons adds in items and increases level of any already on the map

--- Grow Cat/Assets/Scripts/Item Scripts/ItemPhase.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/ItemPhase.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class ItemPhase : MonoBehaviour
 {
     public float maxLevel;
     public float currentLevel = 1;
+
+    public LevelUpManager levelUpManager;
 
     // Start is called before the first frame update
     void Start()
@@ -16,8 +19,11 @@ public class ItemPhase : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //remove this later
         Vector3 scale = new Vector3(currentLevel / 2, currentLevel / 2, currentLevel / 2);
         this.transform.localScale = scale;
+        //remove this later end.
     }
 
     public void LevelUp()
@@ -28,4 +34,14 @@ public class ItemPhase : MonoBehaviour
             currentLevel++;
         }
     }
+
+    public void SpawnOnMap()
+    {
+        levelUpManager.buttonsClicked++;
+        levelUpManager.LevelUpAll(); //level up all objects on the map
+        this.gameObject.SetActive(true); //bring this item onto the map
+     
+    }
+
+
 }
--- Grow Cat/Assets/Scripts/Item Scripts/LevelUpManager.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/LevelUpManager.cs
@@ -4,39 +4,36 @@ using UnityEngine;
 
 public class LevelUpManager : MonoBehaviour
 {
-    ItemPhase[] items;
+    public ItemPhase[] items;
+    public int itemsMaxed, buttonsClicked;
     
 
     // Start is called before the first frame update
     void Start()
     {
-        items = FindObjectsOfType<ItemPhase>();
         Debug.Log(items.Length);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.F))
-        {
-            LevelUpAll();
-        }
+
     }
 
     public void LevelUpAll()
     {
-        Debug.Log("Level up all on scene");
-
-        //run this for every item possible
-       foreach(ItemPhase item in items)
-       {
-            //if the object has been set to active
-            if(item.gameObject.activeInHierarchy)
+       if(items != null)
+        {
+            //run this for every item possible
+            foreach (ItemPhase item in items)
             {
-               // Debug.Log(item.gameObject.name);
-                item.LevelUp();
+                //if the object has been set to active
+                if (item.gameObject.activeInHierarchy)
+                {
+                    // Debug.Log(item.gameObject.name);
+                    item.LevelUp();
+                }
             }
-            
-       }
+        } 
     }
 }
