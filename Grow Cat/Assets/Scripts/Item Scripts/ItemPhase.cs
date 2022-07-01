67c564873ac59dd19648c002030fc691584fe1c8
AIE\s213366 <s213366@students.aie.edu.au>
2022-07-01 11:38:39 +0930

    UPDATED Buttons now use prefabs for each level of each item rather than manipulating just one gameobject. Using test prefabs for now, will update with real models later.

--- Grow Cat/Assets/Scripts/Item Scripts/ItemPhase.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/ItemPhase.cs
@@ -5,25 +5,24 @@ using UnityEngine.EventSystems;
 
 public class ItemPhase : MonoBehaviour
 {
-    public float maxLevel;
-    public float currentLevel = 1;
+    public int maxLevel;
+    public int currentLevel = 1;
 
     public LevelUpManager levelUpManager;
+    ModelSwap modelSwap;
+    GameObject model;
+    PositionAnchor positionAnchor;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        modelSwap = GetComponent<ModelSwap>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //remove this later
-        Vector3 scale = new Vector3(currentLevel / 2, currentLevel / 2, currentLevel / 2);
-        this.transform.localScale = scale;
-        //remove this later end.
     }
 
     public void LevelUp()
@@ -32,6 +31,21 @@ public class ItemPhase : MonoBehaviour
         if(currentLevel < maxLevel)
         {
             currentLevel++;
+            StartCoroutine(InstantiateModel(0.5f));
+        }
+    }
+
+    public IEnumerator InstantiateModel(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+
+        //check there is still a model available
+        if(currentLevel < modelSwap.models.Length)
+        {
+            Destroy(model); //clear previous model
+            model = Instantiate(modelSwap.models[currentLevel].gameObject); //create this objects model at it's current level
+            positionAnchor = model.GetComponentInParent<PositionAnchor>(); //grab the model's anchor script
+            positionAnchor.itemBlock = this.gameObject.transform; //assign the item's location as the models location
         }
     }
 
@@ -40,13 +54,13 @@ public class ItemPhase : MonoBehaviour
         levelUpManager.buttonsClicked++; //tell system we've clicked a button
         levelUpManager.LevelUpAll(); //level up all objects on the map
         this.gameObject.SetActive(true); //bring this item onto the map
+        StartCoroutine(InstantiateModel(0.5f));
 
         //check if all buttons are clicked
-        if(levelUpManager.buttonsClicked == 6)
+        if (levelUpManager.buttonsClicked == 6)
         {
             StartCoroutine(CalculateResult(1));
-        }
-        
+        } 
     }
 
     public IEnumerator CalculateResult(float seconds)
