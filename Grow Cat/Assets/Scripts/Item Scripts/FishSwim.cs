b39ab927252a14b6bcc79c13a4fd3d326f8a1a08
AIE\s214492 <s214492@students.aie.edu.au>
2022-07-06 15:51:01 +0930

    ADDED comments to Fish Swim script

--- Grow Cat/Assets/Scripts/Item Scripts/FishSwim.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/FishSwim.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 [SerializeField]
 public class FishSwim : MonoBehaviour
 {
+    //first location of the fish's path
     public Transform location1;
+    
+    //second location of the fish's path
     public Transform location2;
-    private Transform transferLocation;
+    
+    //the fish's transform value
     public Transform currentLocation;
+
+    //allows you to vary fish speed if you want, if not leave it at 0 or remove
     public float speedBonus;
 
-    public float distance;
+    private Transform transferLocation;
+    private float distance;
 
     // Start is called before the first frame update
     void Start()
@@ -22,18 +29,25 @@ public class FishSwim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //adds to the distance variable over time, rate influenced by speedBonus
         distance = ((distance + Time.deltaTime) + speedBonus/1000) % 1f;
 
+        //once we get above 0.8 (pretty much end of the path)
         if (distance > 0.8)
         {
+            //reset distance value
             distance = 0;
+
+            //flip the fish
             transform.Rotate(0, 180, 0);
+
+            //swap points (move towards other point)
             transferLocation = location1;
             location1 = location2;
             location2 = transferLocation;
         }
 
+        //moves towards point 2 as distance increases
         currentLocation.position = Vector3.Lerp(location1.position, location2.position, distance);
     }
 }
