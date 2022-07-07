5313273695b8d956b1277bca130c484f40b98fcd
AIE\s213366 <s213366@students.aie.edu.au>
2022-07-07 11:44:49 +0930

    Fixed Bell Issues

--- Grow Cat/Assets/Scripts/Item Scripts/BellScript.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/BellScript.cs
@@ -6,12 +6,15 @@ public class BellScript : MonoBehaviour
 {
     GameObject catTower;
     GameObject bellSpawn;
+    public GameObject deadState;
+
 
     // Start is called before the first frame update
     void Start()
     {
         catTower = GameObject.Find("Box_Level5(Clone)"); //if the cat tower is level 5, assign
         bellSpawn = GameObject.Find("Object_Bell"); //assign bell spawn
+
     }
 
     // Update is called once per frame
@@ -25,6 +28,18 @@ public class BellScript : MonoBehaviour
         if (catTower != null) //if the cat tower is levelled enough, the bell will stay at the spawn point
         {
             this.transform.position = bellSpawn.transform.position;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground") //if the bell touches the ground 
+        {
+            GameObject dead = Instantiate(deadState); //create a bell w/out any scripts
+            dead.transform.position = transform.position; //the bell is put where the previous bell was
+            bellSpawn.SetActive(false); //kill the spawner
+            Destroy(this.gameObject); //destroy the old bell
+
         }
     }
 }
