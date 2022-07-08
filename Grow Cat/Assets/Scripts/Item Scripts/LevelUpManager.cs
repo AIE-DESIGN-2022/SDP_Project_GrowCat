34b3f43153f931701794cce0b99640955de54614
AIE\s213366 <s213366@students.aie.edu.au>
2022-07-08 11:11:24 +0930

    ADDED Extras Spawn on Complete

--- Grow Cat/Assets/Scripts/Item Scripts/LevelUpManager.cs
+++ Grow Cat/Assets/Scripts/Item Scripts/LevelUpManager.cs
@@ -8,6 +8,7 @@ public class LevelUpManager : MonoBehaviour
     public ItemPhase[] items;
     public int itemsMaxed, buttonsClicked;
     public GameObject[] pops;
+    public GameObject[] extras;
     BallScript ballScript;
 
     public void FindBall() //find the Ball object when it appears
@@ -51,13 +52,27 @@ public class LevelUpManager : MonoBehaviour
             if(item.currentLevel == item.maxLevel) //display on the buttonText that the object is at max level
             {
                 item.buttonText.text = "Max Level";
+                
             }
 
             else //otherwise display its current level
             {
                 item.buttonText.text = "Level " + item.currentLevel.ToString();
             }
+
+            
         }
+
+        if(itemsMaxed == 6)
+            {
+                foreach(GameObject extra in extras)
+                {
+                    float x = Random.Range(-4, 2);
+                    float z = Random.Range(0, -5);
+                    Vector3 pos = new Vector3(x, 1, z);
+                    Instantiate(extra, pos, extra.transform.rotation);
+                }
+            }
     }
 
     public void CheckForMaxed()
